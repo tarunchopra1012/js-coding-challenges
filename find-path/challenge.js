ff2b3fe9aa3e31598ac858acf453c50642ec5c51
tarunchopra1012 <tarun.chopra10@gmail.com>
2024-07-31 19:58:16 +0530

    Find Path

---
+++ find-path/challenge.js
@@ -0,0 +1,42 @@
+/*
+- Write method findPath
+- Should take two params:
+    - object
+    - keys separated by dots as string
+- Return value if it exists at that path inside the object, else return undefined
+*/
+
+var obj = {
+    a: {
+        b: {
+            c: 12,
+            j: false
+        },
+        k: null
+    }
+};
+
+const findPath = (object, path) => {
+    const keys = path.split('.');
+    let current = object;
+
+    for (let key of keys) {
+        if (current !== 'null' && typeof current === 'object' && key in current) {
+            current = current[key];
+        } else {
+            return 'undefined';
+        }
+    };
+
+    return current;
+};
+
+console.log(findPath(obj, 'a.b.c')); // 12
+console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
+console.log(findPath(obj, 'a.b.d')); // undefined
+console.log(findPath(obj, 'a.c')); // undefined
+console.log(findPath(obj, 'a.b.c.d')); // undefined
+console.log(findPath(obj, 'a.b.c.d.e')); // undefined
+console.log(findPath(obj, 'a.b.j')); //false
+console.log(findPath(obj, 'a.b.j.k')); //undefined
+console.log(findPath(obj, 'a.k')); //null
